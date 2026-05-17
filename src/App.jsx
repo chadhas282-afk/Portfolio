059db9eff9ffeb62ed654228778115babdb784e0
chadhas282-afk <chadhas282@gmail.com>
2026-05-17 11:12:57 +0530

    Add ParallaxText component: enhance velocity factor calculation with transform settings

--- src/App.jsx
+++ src/App.jsx
@@ -79,3 +79,8 @@ const ParallaxText = ({ children, baseVelocity = 100 }) => {
   const scrollVelocity = useVelocity(scrollY);
   const smoothVelocity = useSpring(scrollVelocity, {
     damping: 50,
+    stiffness: 400
+  });
+  const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
+    clamp: false
+  });
