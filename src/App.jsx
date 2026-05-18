b02ef939ad9fd6d6ae9da6f1e6f1cc97aeae55d4
chadhas282-afk <chadhas282@gmail.com>
2026-05-18 10:33:34 +0530

    Enhance FloatingObject component: add rotation animation for improved visual dynamics

--- src/App.jsx
+++ src/App.jsx
@@ -173,3 +173,8 @@ const FloatingObject = ({ color1, color2, delay = 0 }) => {
         x: [0, 100, -50, 0],
         y: [0, -100, 50, 0],
         scale: [1, 1.2, 0.8, 1],
+         rotate: [0, 180, 360],
+      }}
+      transition={{
+        duration: 20,
+        repeat: Infinity,
