0d58e0e49832283b9e659140e99bde56bbfa8a30
chadhas282-afk <chadhas282@gmail.com>
2026-05-19 10:29:48 +0530

    Add particle properties in NeuralNetwork component: initialize velocity and size for particles

--- src/App.jsx
+++ src/App.jsx
@@ -211,3 +211,7 @@ const NeuralNetwork = () => {
       constructor() {
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
+        this.vx = (Math.random() - 0.5) * 0.5;
+        this.vy = (Math.random() - 0.5) * 0.5;
+        this.size = Math.random() * 2;
+      }
